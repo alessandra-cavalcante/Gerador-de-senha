a5118508f710773a4e021d22148ddff06446de0e
Alessandra <alessandra.cavalcantea@outlook.com>
2023-05-11 22:57:19 -0300

    Js finalizado
    Projeto rodando

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("Teste")
+let sliderElement = document.querySelector("#slider");
+let buttomElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset  = "abcdefghijklmnopqrstuvwxyz0123456789!@*ABCDEFGHIJKLMNOPQRSTU";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+    
+    let pass = "";
+
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+
+}
+
+function copyPassword(){
+    navigator.clipboard.writeText(novaSenha);
+    alert("Senha copiada com sucesso!");
+}
+
+
+
